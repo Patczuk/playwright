9a31a97c9c64b1e6a1c0718b1b2db0c73dc72a07
Misha <333mike555@gmail.com>
2023-03-26 11:26:05 +0300

    task 1 completed

--- Playwright/tests/task_1.spec.js
+++ Playwright/tests/task_1.spec.js
@@ -3,11 +3,11 @@ const { test, expect } = require('@playwright/test');
 test('task_1', async ({ page }) => {
   await page.goto('https://www.onliner.by/');
   await expect(page).toHaveTitle(/Onlíner/); // check title
-  //await expect(page.locator('b-main-navigation__text :text("Каталог")')).toHaveCount(1); // by CSS
-  await expect(page.locator("//input[@name='query']")).toHaveCount(1); // by XPATH
-  await expect(page.getByPlaceholder('Поиск в Каталоге.')).toHaveCount(1); // by placeholder
-  await expect(page.getByRole('listitem').filter({ has: page.getByText('Мыши') })).toHaveCount(1); //filter by another locator
-  // await expect(page.getByText('наши соцсети')).toBeVisible(); // filter by text
-  //локатор, который вернёт список элементов
-  //n-й элемент из списка
+  await expect(page.locator('div.auth-bar__item.auth-bar__item--text')).toHaveCount(1); // by CSS кнопка "Вход"
+  await expect(page.locator("//*[@title='Facebook']")).toHaveCount(1); // by XPATH Facebook button
+  await expect(page.getByPlaceholder('Поиск в Каталоге.')).toHaveCount(1); // by placeholder in the search field
+  await expect(page.getByRole('listitem').filter({ has: page.getByText('Мыши') })).toHaveCount(1); //filter by another locator - tab "Мыши"
+  await expect(page.getByText('наши соцсети')).toHaveCount(1); // filter by text, button 'Наши соцсети'
+  await expect(page.locator('//*[@class="b-main-page-news-2__forum-news-list"]/ol/li')).toHaveCount(7); //локатор, который вернёт список элементов - список самых обсуждаемых тем
+  await expect(page.locator('//*[@class="b-main-navigation"]/li').locator('nth=2')).toHaveCount(1); //n-й элемент из списка - вкладка "Автобарахолка", третий элемент в списке
 });
