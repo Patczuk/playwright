517b46802658e27aaa10924d7b3350811c2d016a
Misha <333mike555@gmail.com>
2023-04-03 16:01:04 +0300

    Task_2_draft

--- Task_2/tests/task_2.spec.js
+++ Task_2/tests/task_2.spec.js
@@ -1,6 +1,11 @@
 const { test, expect } = require('@playwright/test');
 
 test('task_2', async ({ page }) => {
-  await page.goto('https://demoqa.com/');
+  await page.goto('https://demoqa.com/automation-practice-form/');
   await expect(page).toHaveTitle(/DEMOQA/); // check title
+  await page.getByPlaceholder('First Name').fill('Misha'); // locator.fill()
+  //await expect(page.locator('//div[@class=]')).toHaveText('Misha'); check the input 'Misha'
+  await page.locator('//label[for="hobbies-checkbox-2"].input[type="checkbox"]').check(); // locator.check()
+  //await expect(page.getByTestId('gender-radio-1')).toBeChecked();
+  // locator.selectOption()
 });
