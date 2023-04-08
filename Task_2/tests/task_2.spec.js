241266acc27af6c535dd406aca6168b3d4a1426f
Misha <333mike555@gmail.com>
2023-04-08 18:33:31 +0300

    step 1

--- Task_2/tests/task_2.spec.js
+++ Task_2/tests/task_2.spec.js
@@ -2,10 +2,15 @@ const { test, expect } = require('@playwright/test');
 
 test('task_2', async ({ page }) => {
   await page.goto('https://demoqa.com/automation-practice-form/');
+
+  await page.waitForTimeout(2000);
+
   await expect(page).toHaveTitle(/DEMOQA/); // check title
-  await page.getByPlaceholder('First Name').fill('Misha'); // locator.fill()
-  //await expect(page.locator('//div[@class=]')).toHaveText('Misha'); check the input 'Misha'
-  await page.locator('//label[for="hobbies-checkbox-2"].input[type="checkbox"]').check(); // locator.check()
-  //await expect(page.getByTestId('gender-radio-1')).toBeChecked();
+
+  await page.locator('id=firstName').fill('Misha'); // locator.fill()
+  await expect(page.locator('id=firstName')).toHaveValue('Misha'); // check the input value 'Misha'
+
+  await page.locator('id=gender-radio-1').check(); // locator.click()
+  await expect(page.locator('id=gender-radio-1')).toBeChecked(); // verify that radio button is clicked
   // locator.selectOption()
 });
