4d2b95edc52a599ac185684844544f8801db31e8
Misha <333mike555@gmail.com>
2023-04-09 12:34:34 +0300

    wtf, it doesn't work

--- Task_2/tests/task_2.spec.js
+++ Task_2/tests/task_2.spec.js
@@ -1,7 +1,8 @@
 const { test, expect } = require('@playwright/test');
 
-test('task_2', async ({ page }) => {
+test('task_2 part_1', async ({ page }) => {
   await page.goto('https://demoqa.com/automation-practice-form/');
+  //await page.pause(); // using for debug
 
   await page.waitForTimeout(2000);
 
@@ -10,7 +11,13 @@ test('task_2', async ({ page }) => {
   await page.locator('id=firstName').fill('Misha'); // locator.fill()
   await expect(page.locator('id=firstName')).toHaveValue('Misha'); // check the input value 'Misha'
 
-  await page.locator('id=gender-radio-1').check(); // locator.click()
-  await expect(page.locator('id=gender-radio-1')).toBeChecked(); // verify that radio button is clicked
-  // locator.selectOption()
+  //await page.locator('id=gender-radio-1').check(); // locator.check()
+  //await expect(page.locator('id=gender-radio-1')).toBeChecked(); // verify that radio button is marked
+
+  //await page.locator('id=react-select-3-input').click();
+  //await page.locator('xpath=//div[text()="select state"]').click();
+  //await page.locator("//[@class='css-1uccc91-singleValue']").selectOption('Haryana'); // locator.selectOption()
+
+  //await page.locator('id=hobbies-checkbox-1').check(); //locator.click()
+  //await expect(page.locator('id=hobbies-checkbox-1')).toBeChecked(); // // verify that radio button is marked
 });
